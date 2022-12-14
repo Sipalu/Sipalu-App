c8a34956f6c763189e704f91472db34b6b660b4c
Sakriya <you@example.com>
2022-12-14 13:08:16 +0545

    created models for notification

--- notification_app/models.py
+++ notification_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Created models for notification
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
